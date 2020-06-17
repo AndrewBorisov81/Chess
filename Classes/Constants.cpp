b451953c18c4c6bf35356177a530cec3a3368490
GithubTestAB <misterABP@gmail.com>
2020-06-17 18:44:57 +0300

    Create Constants

---
+++ Classes/Constants.cpp
@@ -0,0 +1,6 @@
+namespace Conatants
+{
+  // Images
+  extern constexpr auto BOARD_PNG = "chessBoard.png";
+  extern constexpr auto FIGURE_PNG = "blackHorse.png";
+}
---
+++ Classes/Constants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Constants
+{
+  // Images
+  constexpr auto BOARD_PNG = "chessBoard.png";
+  constexpr auto FIGURE_PNG = "blackHorse.png";
+
+  // Game elements setting
+  constexpr auto PLAYER_SPEED = 1.0f;
+  constexpr auto ENEMY_SPEED = 1.0f;
+}
