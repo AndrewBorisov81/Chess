4b1a4bf388b9dc86a32b23d5dfecaa445e7cabe2
GithubTestAB <misterABP@gmail.com>
2020-06-18 09:08:29 +0300

    Added cell size

--- Classes/Constants.cpp
+++ Classes/Constants.cpp
@@ -1,6 +1,8 @@
 namespace Conatants
 {
   // Images
-  extern constexpr auto BOARD_PNG = "chessBoard.png";
-  extern constexpr auto FIGURE_PNG = "blackHorse.png";
+  extern constexpr auto BOARD_PNG("chessBoard.png");
+  extern constexpr auto FIGURE_PNG("blackHorse.png");
+
+  extern constexpr auto CELL_SIZE(25.0f);
 }
--- Classes/Constants.h
+++ Classes/Constants.h
@@ -3,10 +3,9 @@
 namespace Constants
 {
   // Images
-  constexpr auto BOARD_PNG = "chessBoard.png";
-  constexpr auto FIGURE_PNG = "blackHorse.png";
+  constexpr auto BOARD_PNG("chessBoard.png");
+  constexpr auto FIGURE_PNG("blackHorse.png");
 
   // Game elements setting
-  constexpr auto PLAYER_SPEED = 1.0f;
-  constexpr auto ENEMY_SPEED = 1.0f;
+  constexpr auto CELL_SIZE(25.0f);
 }
