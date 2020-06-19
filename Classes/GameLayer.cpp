55910b06217536d271735fc99fc5c9062343faa4
GithubTestAB <misterABP@gmail.com>
2020-06-19 18:54:41 +0300

    Added test figures

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -2,6 +2,7 @@
 #include "Board.h"
 #include "Grid.h"
 #include "Constants.h"
+#include "Figure.h"
 
 USING_NS_CC;
 
@@ -38,6 +39,38 @@ bool GameLayer::init()
   board->addChild(grid, static_cast<int>(ZOrderGame::GRID));
   m_grid = grid;
   grid->setPosition(Vec2(-(Constants::CELL_SIZE * Constants::COLUMNS/2), -(Constants::CELL_SIZE * Constants::ROWS/2)));
+
+  // Create Figure
+  Figure* figure1 = Figure::createFigure(Constants::BLACK_ROOK_PNG);
+  grid->addChild(figure1, static_cast<int>(ZOrderGame::FIGURE));
+  figure1->setPosition(grid->getPointByCell(0, 0));
+
+  // Create Figure
+  Figure* figure2 = Figure::createFigure(Constants::BLACK_HORSE_PNG);
+  grid->addChild(figure2, static_cast<int>(ZOrderGame::FIGURE));
+  figure2->setPosition(grid->getPointByCell(1, 0));
+
+  // Create Figure
+  Figure* figure3 = Figure::createFigure(Constants::BLACK_OFFICER_PNG);
+  grid->addChild(figure3, static_cast<int>(ZOrderGame::FIGURE));
+  figure3->setPosition(grid->getPointByCell(2, 0));
+
+  // Create Figure
+  Figure* figure4 = Figure::createFigure(Constants::BLACK_QUEEN_PNG);
+  grid->addChild(figure4, static_cast<int>(ZOrderGame::FIGURE));
+  figure4->setPosition(grid->getPointByCell(3, 0));
+
+  // Create Figure
+  Figure* figure5 = Figure::createFigure(Constants::BLACK_KING_PNG);
+  grid->addChild(figure5, static_cast<int>(ZOrderGame::FIGURE));
+  figure5->setPosition(grid->getPointByCell(4, 0));
+
+
+  // Create Figure
+  Figure* figure6 = Figure::createFigure(Constants::BLACK_PAWN_PNG);
+  grid->addChild(figure6, static_cast<int>(ZOrderGame::FIGURE));
+  figure6->setPosition(grid->getPointByCell(4, 1));
+
  
   return true;
 }
