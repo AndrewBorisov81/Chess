662716702aa50a4af038b9a086f269d7e2561658
GithubTestAB <misterABP@gmail.com>
2020-06-19 18:54:01 +0300

    Added black figures image

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -3,11 +3,17 @@
 namespace Constants
 {
   // Images
-  extern constexpr auto BOARD_PNG("chessBoard.png");
-  extern constexpr auto FIGURE_PNG("blackHorse.png");
+  extern constexpr auto BOARD_PNG("chessBoard3.png");
+  // Figures
+  extern constexpr auto BLACK_PAWN_PNG("blackPawn3.png");
+  extern constexpr auto BLACK_ROOK_PNG("blackRook3.png");
+  extern constexpr auto BLACK_HORSE_PNG("blackHorse3.png");
+  extern constexpr auto BLACK_OFFICER_PNG("blackOfficer3.png");
+  extern constexpr auto BLACK_QUEEN_PNG("blackQueen3.png");
+  extern constexpr auto BLACK_KING_PNG("blackKing3.png");
 
   // Game elements setting
-  extern constexpr auto CELL_SIZE(100.0f);
+  extern constexpr auto CELL_SIZE(118.0f);
   extern constexpr auto ROWS(8);
   extern constexpr auto COLUMNS(8);
 }
