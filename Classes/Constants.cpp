143d16d67b325d0bb767bd967bde772f739de98e
GithubTestAB <misterABP@gmail.com>
2020-06-23 05:48:03 +0300

    Update data classes contants

--- Classes/Constants.cpp
+++ Classes/Constants.cpp
@@ -4,7 +4,7 @@ namespace Constants
   extern constexpr auto BOARD_PNG("chessBoard.png");
   extern const auto FIGURE_PNG("blackHorse.png");
 
-  extern constexpr auto CELL_SIZE(100.0f);
+  extern constexpr auto CELL_SIZE(118.0f);
   extern constexpr auto ROWS(8);
   extern constexpr auto COLUMNS(8);
 }
