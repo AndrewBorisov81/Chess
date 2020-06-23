42aabce679212604c5b740d5399dd0651198714b
GithubTestAB <misterABP@gmail.com>
2020-06-23 20:53:00 +0300

    Show pictures and add callback

--- Classes/Figure.cpp
+++ Classes/Figure.cpp
@@ -1,15 +1,32 @@
 #include "Figure.h"
 
+#include <string>
+
 USING_NS_CC;
 using namespace cocos2d::ui;
 
-Figure::Figure()
+Figure::Figure() :
+  cocos2d::Sprite::Sprite()
 {
 }
 
-Figure::Figure(const std::string& fileName)
+Figure::Figure(const std::string& fileName) :
+  cocos2d::Sprite::Sprite(),
+  m_fileName(fileName)
 {
-  
+
+}
+
+void Figure::InitFigure()
+{
+  ImageView* imageView = ImageView::create(m_fileName, cocos2d::ui::ImageView::TextureResType::PLIST);
+  m_imageView = imageView;
+  m_imageView->setTouchEnabled(true);
+  m_imageView->Node::setContentSize({ 250, 250 });
+  m_imageView->addClickEventListener([=](Ref*) {
+    m_imageView->setRotation(45);
+  });
+  this->addChild(imageView, 1);
 }
 
 Figure::~Figure()
@@ -20,8 +37,9 @@ Figure* Figure::createFigure(const std::string& fileName)
 {
   Figure* pFigure = new(std::nothrow) Figure(fileName);
 
-  if (pFigure && pFigure->initWithSpriteFrameName(fileName))
+  if (pFigure && pFigure->init())
   {
+    pFigure->InitFigure();
     pFigure->autorelease();
     return pFigure;
   }
@@ -33,4 +51,10 @@ Figure* Figure::createFigure(const std::string& fileName)
   }
   return pFigure;
 }
+
+cocos2d::ui::ImageView* Figure::getImageView()
+{
+  return m_imageView;
+}
+
 
