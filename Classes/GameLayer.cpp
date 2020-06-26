0de82ec78014fbcdb26aa10977d21cec3cc24528
GithubTestAB <misterABP@gmail.com>
2020-06-26 14:02:53 +0300

    Added type figure

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -128,13 +128,13 @@ void GameLayer::createTestFigures()
   Grid* grid = m_grid;
 
   // Create Figure
- /*Figure* figure1 = Figure::createFigure(Constants::BLACK_ROOK_PNG);
+ /*Figure* figure1 = Figure::createFigure(TypeFigure::ROOK, Constants::BLACK_ROOK_PNG);
  grid->addChild(figure1, static_cast<int>(ZOrderGame::FIGURE));
  figure1->setPosition(grid->getPointByCell(0, 0));
  });*/
 
  // Create Figure
-  Figure* figure2 = Figure::createFigure(Constants::BLACK_HORSE_PNG);
+  Figure* figure2 = Figure::createFigure(TypeFigure::HORSE, Constants::BLACK_HORSE_PNG);
   grid->addChild(figure2, static_cast<int>(ZOrderGame::FIGURE));
   figure2->setPosition(grid->getPointByCell(1, 0));
   figure2->setTouchEnabled(true);
@@ -147,22 +147,22 @@ void GameLayer::createTestFigures()
   });
 
   // Create Figure
-  /*Figure* figure3 = Figure::createFigure(Constants::BLACK_OFFICER_PNG);
+  /*Figure* figure3 = Figure::createFigure(TypeFigure::OFFICER, Constants::BLACK_OFFICER_PNG);
   grid->addChild(figure3, static_cast<int>(ZOrderGame::FIGURE));
   figure3->setPosition(grid->getPointByCell(2, 0));
 
   // Create Figure
-  Figure* figure4 = Figure::createFigure(Constants::BLACK_QUEEN_PNG);
+  Figure* figure4 = Figure::createFigure(TypeFigure::QUEEN, Constants::BLACK_QUEEN_PNG);
   grid->addChild(figure4, static_cast<int>(ZOrderGame::FIGURE));
   figure4->setPosition(grid->getPointByCell(3, 0));
 
   // Create Figure
-  Figure* figure5 = Figure::createFigure(Constants::BLACK_KING_PNG);
+  Figure* figure5 = Figure::createFigure(TypeFigure::KING, Constants::BLACK_KING_PNG);
   grid->addChild(figure5, static_cast<int>(ZOrderGame::FIGURE));
   figure5->setPosition(grid->getPointByCell(4, 0));
 
   // Create Figure
-  Figure* figure6 = Figure::createFigure(Constants::BLACK_PAWN_PNG);
+  Figure* figure6 = Figure::createFigure(TypeFigure::PAWN, Constants::BLACK_PAWN_PNG);
   grid->addChild(figure6, static_cast<int>(ZOrderGame::FIGURE));
   figure6->setPosition(grid->getPointByCell(4, 1));
   figure6->activateClickEvent();*/
