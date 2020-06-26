77df9ca60fd42bc6b177694ee98d541fb5475a76
GithubTestAB <misterABP@gmail.com>
2020-06-26 13:03:25 +0300

    Added white figures

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -12,6 +12,13 @@ namespace Constants
   extern constexpr auto BLACK_QUEEN_PNG("blackQueen3.png");
   extern constexpr auto BLACK_KING_PNG("blackKing3.png");
 
+  extern constexpr auto WHITE_PAWN_PNG("whitePawn3.png");
+  extern constexpr auto WHITE_ROOK_PNG("whiteRook3.png");
+  extern constexpr auto WHITE_HORSE_PNG("whiteHorse3.png");
+  extern constexpr auto WHITE_OFFICER_PNG("whiteOfficer3.png");
+  extern constexpr auto WHITE_QUEEN_PNG("whiteQueen3.png");
+  extern constexpr auto WHITE_KING_PNG("whiteKing3.png");
+
   // Game elements setting
   extern constexpr auto CELL_SIZE(118.0f);
   extern constexpr auto ROWS(8);
