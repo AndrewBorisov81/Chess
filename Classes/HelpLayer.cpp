9d29ec56f2c3875cb1d3ff7a979129974dc06ece
GithubTestAB <misterABP@gmail.com>
2020-07-29 16:30:33 +0300

    Created HelpLayer.h

---
+++ Classes/HelpLayer.cpp
@@ -0,0 +1,21 @@
+#include "HelpLayer.h"
+
+USING_NS_CC;
+
+HelpLayer::HelpLayer()
+{
+}
+
+HelpLayer::~HelpLayer()
+{
+}
+
+bool HelpLayer::init()
+{
+  if (!CCLayer::init()) {
+
+    return false;
+  }
+
+  return true;
+}
---
+++ Classes/HelpLayer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "cocos2d.h"
+
+class HelpLayer : public cocos2d::Layer
+{
+public:
+  HelpLayer();
+  virtual ~HelpLayer();
+
+  virtual bool init();
+};
