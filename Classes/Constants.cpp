c8d4b41e0ea930d50ad6f5f290a3a4248e595abe
GithubTestAB <misterABP@gmail.com>
2020-08-02 15:50:29 +0300

    Update Constants

--- Classes/Constants.cpp
+++ Classes/Constants.cpp
@@ -2,15 +2,84 @@
 
 namespace Constants
 {
-  // Images
-  /*extern constexpr auto BOARD_PNG("chessBoard.png");
-  extern const auto FIGURE_PNG("blackHorse.png");
-
-  extern constexpr auto CELL_SIZE(118.0f);
-  extern constexpr auto ROWS(8);
-  extern constexpr auto COLUMNS(8);
-
-  extern constexpr auto EMPTY_SQUARE(nullptr);*/
-
+  // Imgages
   const std::string BOARD_PNG = "chessBoard3.png";
+  const std::string PROMOTION_TABLE = "promotionTable.png";
+
+  // Buttons
+  const std::string UNDO_BUTTON = "undoButtonNormal.png";
+  const std::string UNDO_BUTTON_SELECTED = "undoButtonSelected.png";
+
+  // Figures
+  const std::string BLACK_PAWN_PNG = "blackPawn3.png";
+  const std::string BLACK_ROOK_PNG = "blackRook3.png";
+  const std::string BLACK_KNIGHT_PNG = "blackHorse3.png";
+  const std::string BLACK_BISHOP_PNG = "blackOfficer3.png";
+  const std::string BLACK_QUEEN_PNG = "blackQueen3.png";
+  const std::string BLACK_KING_PNG = "blackKing3.png";
+
+  const std::string WHITE_PAWN_PNG = "whitePawn3.png";
+  const std::string WHITE_ROOK_PNG = "whiteRook3.png";
+  const std::string WHITE_KNIGHT_PNG = "whiteHorse3.png";
+  const std::string WHITE_BISHOP_PNG = "whiteOfficer3.png";
+  const std::string WHITE_QUEEN_PNG = "whiteQueen3.png";
+  const std::string WHITE_KING_PNG = "whiteKing3.png";
+
+  // Game elements setting
+  const float CELL_SIZE(118.0f);
+  const int ROWS(8);
+  const int COLUMNS(8);
+  Figure* EMPTY_SQUARE(nullptr);
+
+  // This represents the pieces on the board.
+ // Keep in mind that pieces[0][0] represents A1
+ // pieces[1][1] represents B2 and so on.
+ // Numbers with a minus sign are white
+ /*const int INITIAL_FIGURES_BOARD[8][8] =
+               { 1, 2, 3, 4, 5, 3, 2, 1,
+                 6, 6, 6, 6, 6, 6, 6, 6,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                -6,-6,-6,-6,-6,-6,-6,-6,
+                -1,-2,-3,-4,-5,-3,-2,-1
+               };*/
+
+               // Debug
+               // Castling
+               /*const int INITIAL_FIGURES_BOARD[8][8] =
+               { 1, 0, 0, 4, 5, 0, 0, 1,
+                 6, 6, 6, 6, 6, 6, 6, 6,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                -6,-6,-6,-6,-6,-6,-6,-6,
+                -1, 0, 0, 0,-5, 0, 0,-1
+               };*/
+
+               // Checkmate
+               /*const int INITIAL_FIGURES_BOARD[8][8] =
+               { 1, 2, 3, 4, 5, 3, 2, 1,
+                 6, 6, 6, 6, 0, 6, 6, 6,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 6, 0, 0, 0,
+                 0, 0, 0, 0,-6, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0,
+                -6,-6,-6,-6,-5,-6,-6,-6,
+                -1,-2,-3,-4, 0,-3,-2,-1
+               };*/
+
+               // Debug promotion
+  const int INITIAL_FIGURES_BOARD[8][8] =
+  { 0, 0, 0, 0, 5, 3, 2, 1,
+    0, 0, 0, 6, 6, 6, 6, 6,
+   -6, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0,-6,-6,-6,-6,-6,-6,-6,
+   -1,-2,-3,-4,-5,-3,-2,-1
+  };
 }
--- Classes/Constants.h
+++ Classes/Constants.h
@@ -2,68 +2,52 @@
 
 #include <string>
 
+class Figure;
+
 namespace Constants
 {
   // Images
   extern const std::string BOARD_PNG;
-  // Figures
-  extern constexpr auto BLACK_PAWN_PNG("blackPawn3.png");
-  extern constexpr auto BLACK_ROOK_PNG("blackRook3.png");
-  extern constexpr auto BLACK_KNIGHT_PNG("blackHorse3.png");
-  extern constexpr auto BLACK_BISHOP_PNG("blackOfficer3.png");
-  extern constexpr auto BLACK_QUEEN_PNG("blackQueen3.png");
-  extern constexpr auto BLACK_KING_PNG("blackKing3.png");
+  extern const std::string PROMOTION_TABLE;
 
-  extern constexpr auto WHITE_PAWN_PNG("whitePawn3.png");
-  extern constexpr auto WHITE_ROOK_PNG("whiteRook3.png");
-  extern constexpr auto WHITE_KNIGHT_PNG("whiteHorse3.png");
-  extern constexpr auto WHITE_BISHOP_PNG("whiteOfficer3.png");
-  extern constexpr auto WHITE_QUEEN_PNG("whiteQueen3.png");
-  extern constexpr auto WHITE_KING_PNG("whiteKing3.png");
+  // Button
+  extern const std::string UNDO_BUTTON_NORMAL;
+  extern const std::string UNDO_BUTTON_SELECTED;
+
+  // Figures
+  extern const std::string BLACK_PAWN_PNG;
+  extern const std::string BLACK_ROOK_PNG;
+  extern const std::string BLACK_KNIGHT_PNG;
+  extern const std::string BLACK_BISHOP_PNG;
+  extern const std::string BLACK_QUEEN_PNG;
+  extern const std::string BLACK_KING_PNG;
+
+  extern const std::string WHITE_PAWN_PNG;
+  extern const std::string WHITE_ROOK_PNG;
+  extern const std::string WHITE_KNIGHT_PNG;
+  extern const std::string WHITE_BISHOP_PNG;
+  extern const std::string WHITE_QUEEN_PNG;
+  extern const std::string WHITE_KING_PNG;
 
   // Game elements setting
-  extern constexpr auto CELL_SIZE(118.0f);
-  extern constexpr auto ROWS(8);
-  extern constexpr auto COLUMNS(8);
-  extern constexpr auto EMPTY_SQUARE(nullptr);
+  extern const float CELL_SIZE;
+  extern const int ROWS;
+  extern const int COLUMNS;
+  extern Figure* EMPTY_SQUARE;
 
   // This represents the pieces on the board.
   // Keep in mind that pieces[0][0] represents A1
   // pieces[1][1] represents B2 and so on.
   // Numbers with a minus sign are white
-  constexpr int INITIAL_FIGURES_BOARD[8][8] =
-                { 1, 2, 3, 4, 5, 3, 2, 1,
-                  6, 6, 6, 6, 6, 6, 6, 6,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0, 0, 0, 0, 
-                 -6,-6,-6,-6,-6,-6,-6,-6,
-                 -1,-2,-3,-4,-5,-3,-2,-1
-                };
+  //*extern const int INITIAL_FIGURES_BOARD[8][8];
 
   // Debug
   // Castling
-  /*constexpr int INITIAL_FIGURES_BOARD[8][8] =
-  { 1, 0, 0, 4, 5, 0, 0, 1,
-    6, 6, 6, 6, 6, 6, 6, 6,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-   -6,-6,-6,-6,-6,-6,-6,-6,
-   -1, 0, 0, 0,-5, 0, 0,-1
-  };*/
+  //extern const int INITIAL_FIGURES_BOARD[8][8];
 
   // Checkmate
-  /*constexpr int INITIAL_FIGURES_BOARD[8][8] =
-  { 1, 2, 3, 4, 5, 3, 2, 1,
-    6, 6, 6, 6, 0, 6, 6, 6,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 6, 0, 0, 0,
-    0, 0, 0, 0,-6, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-   -6,-6,-6,-6,-5,-6,-6,-6,
-   -1,-2,-3,-4, 0,-3,-2,-1
-  };*/ 
+  //extern const int INITIAL_FIGURES_BOARD[8][8];
+
+  // Debug promotion
+  extern const int INITIAL_FIGURES_BOARD[8][8];
 }
