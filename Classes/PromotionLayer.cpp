89b3d417869f1ffab4e26839105e4f82eb62e553
GithubTestAB <misterABP@gmail.com>
2020-08-04 11:22:55 +0300

    Added show, showFigures, hideFigures

--- Classes/PromotionLayer.cpp
+++ Classes/PromotionLayer.cpp
@@ -19,13 +19,28 @@ bool PromotionLayer::init()
     return false;
   }
 
+  m_imageFigureScale = 1.5;
+
+  // Load the Sprite Sheet
+  auto spritecache = SpriteFrameCache::getInstance();
+  spritecache->addSpriteFramesWithFile("texture.plist");
+
   // Table image
   auto tableSprite = Sprite::createWithSpriteFrameName(Constants::PROMOTION_TABLE);
+  m_table = tableSprite;
   Size tableSize = tableSprite->getContentSize();
   m_tableSize = tableSize;
   this->addChild(tableSprite, 1);
 
-  createFigures(1, true);
+  // Create white figures
+  createFigures(true, tableSize);
+
+  // Create black figures
+  createFigures(false, tableSize);
+
+  hideFigures();
+
+  show(false);
 
   return true;
 }
@@ -35,36 +50,134 @@ cocos2d::Size PromotionLayer::getTableSize()
   return m_tableSize;
 }
 
-void PromotionLayer::show()
+void PromotionLayer::show(bool isWhite)
 {
+  std::vector<Menu*> itemFigures;
+
+  m_isWhite = isWhite;
+
+  if (m_isWhite)
+    itemFigures = m_whiteFigures;
+  else
+    itemFigures = m_blackFigures;
+
+  for (int i = 0; i < itemFigures.size(); i++)
+  {
+    Menu* figures = itemFigures[i];
+    figures->setOpacity(255);
+  }
+
+  m_table->setOpacity(255);
 }
 
 void PromotionLayer::hide()
 {
+  hideFigures();
+  
+  m_table->setOpacity(0);
+}
+
+void PromotionLayer::showFigures(bool isWhite)
+{
+  std::vector<Menu*> itemFigures;
+
+  if (m_isWhite)
+    itemFigures = m_whiteFigures;
+  else
+    itemFigures = m_blackFigures;
+
+  for (int i = 0; i < itemFigures.size(); i++)
+  {
+    Menu* figures = itemFigures[i];
+    figures->setOpacityModifyRGB(255);
+  }
+}
+
+void PromotionLayer::hideFigures()
+{
+  for (int i = 0; i < m_whiteFigures.size(); i++)
+  {
+    Menu* figures = m_whiteFigures[i];
+    figures->setOpacity(0);
+  }
+
+  for (int i = 0; i < m_blackFigures.size(); i++)
+  {
+    Menu* figures = m_blackFigures[i];
+    figures->setOpacity(0);
+  }
 }
 
 void PromotionLayer::figureCallback(cocos2d::Ref * pSender)
 {
-  bool stop = true;
+  
 }
 
-void PromotionLayer::createFigures(int typeFigure, bool isWhite)
+void PromotionLayer::createFigures(bool isWhite, Size tableSize)
 {
-  std::string pngName = (isWhite)? Constants::WHITE_ROOK_PNG : Constants::BLACK_ROOK_PNG;
+  std::vector<Menu*> itemFigures;
+  std::vector<Rect> rectFigures;
+
+  float sumSizeWidthFigures{ 0 };
+
+  if (isWhite && m_whiteFigures.size() > 0) return;
+  if (!isWhite && m_blackFigures.size() > 0) return;
+
+  for (int i = 1; i < 5; i++)
+  {
+    Rect curRect;
+
+    Menu* figure = createFigure(i, isWhite, curRect);
+
+    sumSizeWidthFigures += curRect.size.width;
+
+    rectFigures.push_back(curRect);
+
+    itemFigures.push_back(figure);
+  }
+
+  if (isWhite) m_whiteFigures = itemFigures;
+  if (!isWhite) m_blackFigures = itemFigures;
+
+  float deltaShift;
+  Rect figureRect = rectFigures[0];
+  deltaShift = 0.3 * figureRect.size.width;
+
+  float initPosX = (tableSize.width - sumSizeWidthFigures)/2;
+  Menu* f1 = itemFigures[0];
+  f1->setPosition(Vec2(initPosX - tableSize.width/2, 0));
+
+  // Set position
+  for (int i = 0; i < itemFigures.size() - 1; i++)
+  {
+    Menu* prevFigure = itemFigures[i];
+    Menu* curFigure = itemFigures[i + 1];
+
+    Rect rectPrevFigure = rectFigures[i];
+
+    Vec2 prevPosFigure = prevFigure->getPosition();
+
+    curFigure->setPosition(prevPosFigure.x + rectPrevFigure.size.width + deltaShift, prevPosFigure.y);
+  }
+}
+
+Menu* PromotionLayer::createFigure(int typeFigure, bool isWhite, Rect& figureRect)
+{
+  std::string pngName = (isWhite) ? Constants::WHITE_ROOK_PNG : Constants::BLACK_ROOK_PNG;
 
   // 2 - KNIGHT; 3 - BISHOP; 4 - QWEEN 
   switch (typeFigure) {
-    case 2:
-      pngName = (isWhite) ? Constants::WHITE_KNIGHT_PNG : Constants::BLACK_KNIGHT_PNG;
-      break;
+  case 2:
+    pngName = (isWhite) ? Constants::WHITE_KNIGHT_PNG : Constants::BLACK_KNIGHT_PNG;
+    break;
 
-    case 3:
-      pngName = (isWhite) ? Constants::WHITE_BISHOP_PNG : Constants::BLACK_KING_PNG;
-      break;
+  case 3:
+    pngName = (isWhite) ? Constants::WHITE_BISHOP_PNG : Constants::BLACK_KING_PNG;
+    break;
 
-    case 4:
-      pngName = (isWhite) ? Constants::WHITE_QUEEN_PNG : Constants::BLACK_QUEEN_PNG;
-      break;
+  case 4:
+    pngName = (isWhite) ? Constants::WHITE_QUEEN_PNG : Constants::BLACK_QUEEN_PNG;
+    break;
   }
 
   auto figure = MenuItemImage::create(
@@ -72,7 +185,12 @@ void PromotionLayer::createFigures(int typeFigure, bool isWhite)
     pngName,
     CC_CALLBACK_1(PromotionLayer::figureCallback, this));
 
-  figure->setScale(1.5);
+  figure->setNormalSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName(pngName));
+  figure->setSelectedSpriteFrame(SpriteFrameCache::getInstance()->getSpriteFrameByName(pngName));
+  figure->setScale(m_imageFigureScale);
+
+  Rect rect = figure->rect();
+  figureRect = rect;
 
   /*closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
     origin.y + closeItem->getContentSize().height / 2));*/
@@ -81,4 +199,6 @@ void PromotionLayer::createFigures(int typeFigure, bool isWhite)
   auto menu = Menu::create(figure, NULL);
   menu->setPosition(Vec2::ZERO);
   this->addChild(menu, 100);
+
+  return menu;
 }
--- Classes/PromotionLayer.h
+++ Classes/PromotionLayer.h
@@ -2,6 +2,8 @@
 
 #include <cocos2d.h>
 
+#include <vector>
+
 class PromotionLayer : public cocos2d::Layer
 {
 public:
@@ -12,14 +14,25 @@ public:
 
   cocos2d::Size getTableSize();
 
-  void show();
+  void show(bool isWhite);
   void hide();
+
+  void showFigures(bool isWhite);
+  void hideFigures();
 
   // a selector callback
   void figureCallback(cocos2d::Ref* pSender);
 
-  void createFigures(int TypeFigure, bool isWhite); 
+  void createFigures(bool isWhite, cocos2d::Size tableSize); 
+  cocos2d::Menu* createFigure(int typeFigure, bool isWhite, cocos2d::Rect& figureRect);
 
 private:
+  cocos2d::Sprite* m_table{ nullptr };
   cocos2d::Size m_tableSize;
+  float m_imageFigureScale{ 0 };
+
+  std::vector<cocos2d::Menu*> m_whiteFigures;
+  std::vector<cocos2d::Menu*> m_blackFigures;
+
+  bool m_isWhite{ true };
 };
