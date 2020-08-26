91c86e72ec0b0fd211713c45987e184114874cc6
GithubTestAB <misterABP@gmail.com>
2020-08-26 20:37:01 +0300

    Added Piece

---
+++ Classes/Piece.cpp
@@ -0,0 +1,101 @@
+#include "Piece.h"
+#include "Constants.h"
+
+#include <string>
+
+USING_NS_CC;
+using namespace cocos2d::ui;
+
+Piece::Piece() :
+  cocos2d::ui::ImageView()
+{
+}
+
+Piece::Piece(int type, bool isWhite)
+  :
+  m_isWhite(isWhite),
+  cocos2d::ui::ImageView()
+{
+
+}
+
+Piece::Piece(int type, bool isWhite, const std::string& fileName)
+  :
+  m_isWhite(isWhite),
+  m_fileName(fileName),
+  cocos2d::ui::ImageView()
+{
+
+}
+
+Piece::~Piece()
+{
+}
+
+/*Piece::Piece(const Piece& Piece)
+{
+  this->init(Piece._textureFile, cocos2d::ui::ImageView::TextureResType::PLIST);
+  this->m_type = Piece.m_type;
+  this->m_isWhite = Piece.m_isWhite;
+  this->autorelease();
+}*/
+
+Piece::Piece(const Piece& Piece)
+{
+  this->init(Piece.m_fileName, cocos2d::ui::ImageView::TextureResType::PLIST);
+  this->m_type = Piece.m_type;
+  this->m_isWhite = Piece.m_isWhite;
+  //this->autorelease();
+}
+
+Piece* Piece::createPiece(int type, bool isWhite, std::string fileName)
+{
+  //Piece* pPiece = new(std::nothrow) Piece(type, isWhite);
+  Piece* pPiece = new(std::nothrow) Piece(type, isWhite, fileName);
+
+  if (pPiece && pPiece->init(fileName, cocos2d::ui::ImageView::TextureResType::PLIST))
+  {
+    pPiece->autorelease();
+    return pPiece;
+  }
+  else
+  {
+    delete pPiece;
+    pPiece = nullptr;
+    return nullptr;
+  }
+  return pPiece;
+}
+
+bool Piece::isWhite()
+{
+  return m_isWhite;
+}
+
+void Piece::setType(TypePiece type)
+{
+  m_type = type;
+}
+
+void Piece::setIsWhite(bool isWhite)
+{
+  m_isWhite = isWhite;
+}
+
+cocos2d::Rect Piece::getRect()
+{
+  auto s = this->getContentSize();
+  return Rect(-s.width / 2, -s.height / 2, s.width, s.height);
+}
+
+Piece* Piece::clonePiece()
+{
+  return new Piece(*this);
+}
+
+TypePiece Piece::getType()
+{
+  return m_type;
+}
+
+
---
+++ Classes/Piece.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "cocos2d.h"
+#include "ui/UIImageView.h"
+
+enum class TypePiece
+{
+  ROOK = 1,
+  KNIGHT = 2,
+  BISHOP = 3,
+  QUEEN = 4,
+  KING = 5,
+  PAWN = 6
+};
+
+class Piece : public cocos2d::ui::ImageView
+{
+public:
+  Piece();
+  Piece(int type, bool isWhite);
+  Piece(int type, bool isWhite, const std::string& fileName);
+  ~Piece();
+
+  // Copy constructor
+  Piece(const Piece& Piece);
+
+  static Piece* createPiece(int type, bool isWhite, std::string fileName);
+
+  TypePiece getType();
+  bool isWhite();
+
+  void setType(TypePiece type);
+  void setIsWhite(bool isWhite);
+
+  cocos2d::Rect getRect();
+
+  Piece* clonePiece();
+
+private:
+  TypePiece m_type{ TypePiece::PAWN };
+  bool m_isWhite{ true };
+
+  std::string m_fileName;
+};
