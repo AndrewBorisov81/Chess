137163b2a466e3f62fda54f87df510f7672e84ee
GithubTestAB <misterABP@gmail.com>
2020-09-11 18:06:54 +0300

    Update isMoveValide function

--- Classes/PieceMoveLogic.cpp
+++ Classes/PieceMoveLogic.cpp
@@ -33,7 +33,7 @@ bool PieceMoveLogic::init()
   return true;
 }
 
-bool PieceMoveLogic::isMoveValid(Piece* piece, Position present, Position future, EnPassant* S_enPassant, Castling* S_castling, Promotion* S_promotion)
+bool PieceMoveLogic::isMoveValid(Position present, Position future, EnPassant* S_enPassant, Castling* S_castling, Promotion* S_promotion)
 {
   bool bValid = false;
 
