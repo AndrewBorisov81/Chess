351f477ff0ea3c0cd005c52a30153213cc0f43f6
GithubTestAB <misterABP@gmail.com>
2020-09-17 10:01:43 +0300

    Added PromptElement

---
+++ Classes/PromptElement.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "cocos2d.h"
+
+enum class TypePrompt
+{
+  PROMPT_RECT,
+  PROMPT_CIRCLE
+};
+
+enum class ColorPrompt
+{
+  GREEN,
+  RED
+};
+
+class PromptElement: public cocos2d::Node
+{
+public:
+  PromptElement();
+  PromptElement(TypePrompt typePrompt);
+  virtual ~PromptElement();
+
+  virtual bool init();
+
+  static PromptElement* createPromptElement(TypePrompt typePrompt);
+
+  void show();
+  void hide();
+
+  void setColor(ColorPrompt colorPrompt);
+
+  TypePrompt getTypePrompt();
+  void setTypePrompt(TypePrompt typeElement);
+
+private:
+  TypePrompt m_typePrompt{ TypePrompt::PROMPT_RECT };
+  
+  ColorPrompt m_colorPrompt{ ColorPrompt::GREEN };
+
+  cocos2d::Sprite* m_greenRect{ nullptr };
+  cocos2d::Sprite* m_redRect{ nullptr };
+  cocos2d::Sprite* m_circle{ nullptr };
+};
