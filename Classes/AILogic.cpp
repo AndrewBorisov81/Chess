06f0e8ab034aded209bb98f4a24fa8062a5f45fc
GithubTestAB <misterABP@gmail.com>
2020-09-28 14:52:38 +0300

    Added AILogic

---
+++ Classes/AILogic.cpp
@@ -0,0 +1,24 @@
+#include "AILogic.h"
+
+AILogic::AILogic()
+{
+
+}
+
+AILogic::~AILogic()
+{
+}
+
+void AILogic::ChooseFigure()
+{
+
+}
+
+void AILogic::PlaceFigure()
+{
+}
+
+void AILogic::SwitchPawnWithFigure()
+{
+
+}
---
+++ Classes/AILogic.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class AILogic
+{
+public:
+  AILogic();
+  ~AILogic();
+
+  void ChooseFigure();
+  void PlaceFigure();
+  void SwitchPawnWithFigure();
+
+private:
+
+};
