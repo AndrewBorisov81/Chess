fdad8b5c27a019fd0827b0c23063ed2ecb878d1f
GithubTestAB <misterABP@gmail.com>
2020-10-15 13:52:31 +0300

    Added PieceMove

---
+++ Classes/PieceMove.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "LogicData.h"
+
+#include <variant>
+
+struct SimpleT { Position from, to; int piece; };
+struct CaptureT { Position from, to; int piece; bool enpassant; };
+struct CastleT { Position king_from, king_to, castle_from, castle_to;
+                 bool kingSide; };
+struct PromotionT { Position from, to; int typeBefore, typeAfter; };
+struct IntendedT { Position from, to; int piece; };
+
+class PieceMove
+{
+public:
+  std::variant<SimpleT, CaptureT, CastleT, PromotionT, IntendedT> details;
+
+  bool isSimple() const { return details.index() == 0; };
+  bool isCapturing() const { return details.index() == 1; };
+  bool isCastling() const { return details.index() == 2; };
+  bool isPromotion() const { return details.index() == 3; };
+  bool isIntendedT() const { return details.index() == 4; };
+};
