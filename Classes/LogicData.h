52acca9969636f58ebdd69950840ed35cb647626
GithubTestAB <misterABP@gmail.com>
2020-10-15 13:57:13 +0300

    Remove Move struct

--- Classes/LogicData.h
+++ Classes/LogicData.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <variant>
-
 enum class Player
 {
   WHITE_PLAYER = 0,
@@ -66,35 +64,6 @@ struct UnderAttack
   bool bUnderAttack;
   int iNumAttackers;
   Attacker attacker[9]; //maximum theorical number of attackers
-};
-
-struct Move
-{
-  int iPiece;
-  Position from;
-  Position to;
-
-  EnPassant en_passant;
-  Castling castling;
-  Promotion promotion;
-};
-
-struct SimpleT { int fromx, fromy, tox, toy; };
-struct CaptureT { int fromx, fromy, tox, toy; bool enpassant; };
-struct CastleT { bool kingSide; };
-
-class ChessMove
-{
-  std::variant<SimpleT, CaptureT, CastleT> m_details;
-
-  /*int iPiece;
-  Player turn;
-  Position from;
-  Position to;*/
-
-  bool isSimple() const { return m_details.index() == 0; };
-  bool isCapturing() const { return m_details.index() == 1; };
-  bool isCastling() const { return m_details.index() == 2; };
 };
 
 struct Undo
