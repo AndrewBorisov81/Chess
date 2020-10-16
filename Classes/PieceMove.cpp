997167c3bfe394777da360f277fb1134068c42c5
GithubTestAB <misterABP@gmail.com>
2020-10-16 07:23:44 +0300

    Added PieceMove.cpp

---
+++ Classes/PieceMove.cpp
@@ -0,0 +1,25 @@
+#include "PieceMove.h"
+
+void PieceMove::getMoveData(MoveData& moveData)
+{
+  if (isSimple())
+  {
+
+  }
+  else if (isCapturing())
+  {
+
+  }
+  else if (isCastling())
+  {
+
+  }
+  else if (isPromotion())
+  {
+
+  }
+  else if (isIntended())
+  {
+
+  }
+}
